e913a13eb9d35b3072e507a8326bba6d8e23b597
Salma Berechid <berechidsalma50@gmail.com>
2023-07-13 07:50:33 -0700

    ad

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity
+ * Return:1
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
